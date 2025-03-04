671a426ff107a3603a2bebfbbfd064171bcee04e
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-03-04 12:31:26 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBVXHVTaAE+1EbsQ5+n//NaDo1kca+syeYje5aSAJ7sR61bh93brmHNZl6s=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF25
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpSI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZks0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
-::ZQ05rAF9IAHYFVzEqQISOg5ASQKNLmi/Zg==
-::eg0/rx1wNQPfEVWB+kM9LVsJDAaQKX+/ALoO6+216vKCwg==
-::fBEirQZwNQPfEVWB+kM9LVsJDCqsE1j6JKcJ/O394PiEpy0=
-::cRolqwZ3JBvQF1fEqQIRJltRTRebfHy7H/UI56j5/f+Tpw0bW/A8fc/OybGcYPAS+VbzeoQpxTpVlosbBRVccQW4IG8=
-::dhA7uBVwLU+EWHeF4EoxJlt6SRCVM3mxSvU1xsub
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATEphJifVt6TRCKPWX6KLAI/+fp5KbHi2M+NA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBVXHVTaAE+/Fb4I5/jH3/iIqEgeQPEDWp3OzrGIL/IQ73nmZ4M503xVisgJFFVdZhfL
-::YB416Ek+Zm8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Bruteforce X - NNOS Apps
 setlocal enabledelayedexpansion
